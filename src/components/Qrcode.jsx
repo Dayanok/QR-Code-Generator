f64dacf916bc6d71ffdf5278c3fa511a80afe2b3
Dena Hadad <dena@Denas-MacBook-Air.local>
2022-06-14 16:26:39 +0300

    Second commit

---
+++ src/components/Qrcode.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import QRCode from 'qrcode';
+import {Button} from '@mui/material';
+
+function Qrcode() {
+
+    const[url, setUrl] = useState('')
+    const[qr, setQr] = useState('')
+    const GenerateQRCode = () =>{
+        QRCode.toDataURL(url, {
+            width: 800,
+            margin:2,
+            color: {
+                //dark: '#33583FF',
+                //light: '#EEEEEEFF'
+            }
+        }, (err, url) => {
+            if(err) return console.error(err);
+            console.log(url);
+            setQr(url)
+        })
+    }
+  return (
+    <div className="app">
+        <h1>QR Generator</h1>
+        <input
+            type="text"
+            placeholder="e.g. http:google.com"
+            value={url}
+            onChange={e => setUrl(e.target.value)}/>
+
+        <Button variant="contained" onClick={GenerateQRCode}>Generate</Button>
+
+        {qr && <>
+            <img src={qr} alt="img"/>
+            <Button variant="contained" color='success' href={qr} download="qrcode.png">Download</Button>
+        </>}
+    </div>
+  )
+}
+
+export default Qrcode
